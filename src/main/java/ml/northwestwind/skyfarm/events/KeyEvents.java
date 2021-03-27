2efcbca9e4dd1959a0773ccca1cbecb65996dd3f
NorthWestWind <wsyn148@gmail.com>
2021-03-27 19:05:17 +0800

    Update KeyEvents.java

--- src/main/java/ml/northwestwind/skyfarm/events/KeyEvents.java
+++ src/main/java/ml/northwestwind/skyfarm/events/KeyEvents.java
@@ -7,13 +7,16 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(modid = SkyFarm.MOD_ID)
 public class KeyEvents {
     @SubscribeEvent
     public static void keyInput(final InputEvent.KeyInputEvent event) {
         Minecraft mc = Minecraft.getInstance();
-        if (mc.options.keyShift.consumeClick()) {
+        if (mc.options.keyShift.consumeClick() && event.getAction() == GLFW.GLFW_PRESS) {
+            LogManager.getLogger().info("Growing tree");
             SkyFarmPacketHandler.INSTANCE.sendToServer(new CPlayerGrowPacket());
         }
     }
