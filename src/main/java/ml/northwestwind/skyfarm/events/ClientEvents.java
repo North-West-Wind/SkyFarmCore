80e46417e38507d192bdf2724246e9bf614fadd1
NorthWestWind <wsyn148@gmail.com>
2021-05-26 21:01:45 +0800

    Stage menu key notification

--- src/main/java/ml/northwestwind/skyfarm/events/ClientEvents.java
+++ src/main/java/ml/northwestwind/skyfarm/events/ClientEvents.java
@@ -7,9 +7,16 @@ import ml.northwestwind.skyfarm.packet.message.CPlayerGrowPacket;
 import ml.northwestwind.skyfarm.screen.GameStageScreen;
 import ml.northwestwind.skyfarm.screen.VoteScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.network.play.server.SChatPacket;
+import net.minecraft.util.Util;
+import net.minecraft.util.text.ChatType;
+import net.minecraft.util.text.IFormattableTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
@@ -17,7 +24,7 @@ import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(modid = SkyFarm.MOD_ID, value = Dist.CLIENT)
-public class KeyEvents {
+public class ClientEvents {
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void keyInput(final InputEvent.KeyInputEvent event) {
         Minecraft mc = Minecraft.getInstance();
@@ -37,5 +44,18 @@ public class KeyEvents {
             if (mc.screen instanceof VoteScreen) mc.setScreen(null);
             else mc.setScreen(new VoteScreen());
         }
+    }
+
+    @SubscribeEvent
+    public static void entityJoinWorld(final EntityJoinWorldEvent event) {
+        if (!(event.getEntity() instanceof ClientPlayerEntity)) return;
+        ClientPlayerEntity player = (ClientPlayerEntity) event.getEntity();
+        if (Minecraft.getInstance().player == null || !player.getUUID().equals(Minecraft.getInstance().player.getUUID())) return;
+        Minecraft.getInstance().gui.handleChat(
+                ChatType.SYSTEM,
+                new TranslationTextComponent(
+                        "tip.skyfarm.stageMenu",
+                        ((IFormattableTextComponent) KeyBindings.stageMenu.getTranslatedKeyMessage()).withStyle(TextFormatting.AQUA)),
+                Util.NIL_UUID);
     }
 }
